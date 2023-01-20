d05c922cf323ddf82025b85ef048072d10fb1415
Eliot <efinch@star.sr.bham.ac.uk>
2023-01-20 17:54:51 +0000

    Update __init__.py

--- fdringdown/__init__.py
+++ fdringdown/__init__.py
@@ -1,1 +1,6 @@
-from .waveforms import ringdown
+from .waveforms import ringdown, wavelet_sum, wavelet_ringdown_sum
+from .interferometer import interferometer
+
+from .prior import prior
+from .likelihood import likelihood
+from .posterior import posterior
